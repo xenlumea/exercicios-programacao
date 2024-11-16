d16e23fb25c95cbce73485853a07024c6fa65100
Emanuel Costa Conrado <dev.conradoemanuelcosta@gmail.com>
2024-11-16 13:17:40 -0300

    bee1001

---
+++ beecrowd/bee1001/bee1001.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int A, B;
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    int X = A + B;
+    printf("X = %d\n", X);
+
+    return 0;
+}
---
+++ beecrowd/bee1001/bee1001.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+    int A, B;
+    std::cin >> A;
+    std::cin >> B;
+
+    int X = A + B;
+
+    std::cout << "X = " << X << "\n";
+    return 0;
+}
